96e3789297e9818f01fecd5369985bb9fdb471fe
Breesho <felix.noel3@orange.fr>
2020-05-13 12:34:16 +0200

    maj

---
+++ assets/js/script.js
@@ -0,0 +1,105 @@
+let symbolplayer = document.querySelectorAll('.symbol-player');
+let symbolbot = document.querySelectorAll('.symbol-bot');
+let start = document.querySelectorAll('.start-battle');
+let resultat = document.querySelector('.resultat');
+
+let playerchoice;
+let botchoice;
+
+let victory = document.querySelector('.victoire');
+let lose = document.querySelector('.defaite');
+let draw = document.querySelector('.egalite');
+
+
+
+console.log(symbolplayer);
+console.log(symbolbot);
+console.log(start);
+console.log(resultat);
+
+
+function random() {
+  return Math.floor(Math.random() * symbolbot.length);
+ }
+// console.log(random());
+ 
+
+
+
+
+function resetbtn (index , bg, border) {
+  for(i = 0; i < symbolplayer.length; i++) {
+    symbolplayer[i].style.backgroundColor = '';
+    symbolplayer[i].style.border = '';
+  }
+  symbolplayer[index].style.backgroundColor = bg;
+  symbolplayer[index].style.border = border;
+}
+
+symbolplayer[0].addEventListener('click', function(e) {
+  playerchoice = 0;
+  resetbtn(0,'#153243', 'solid 6px #76d495');
+
+})
+
+symbolplayer[1].addEventListener('click', function(e) {
+  playerchoice = 1;
+  resetbtn(1,'#153243', 'solid 6px #76d495');
+})
+
+symbolplayer[2].addEventListener('click', function(e) {
+  playerchoice = 2;
+  resetbtn(2,'#153243', 'solid 6px #76d495');
+})
+
+
+
+
+
+function resetbotcolor (index , bg, border) {
+  for(i = 0; i < symbolbot.length; i++) {
+    symbolbot[i].style.backgroundColor = '';
+    symbolbot[i].style.border = '';
+  }
+  symbolbot[index].style.backgroundColor = bg;
+  symbolbot[index].style.border = border;
+}
+
+
+
+
+
+myButton.addEventListener("click", function () {
+  let botchoice = random();
+  console.log('player : ' + playerchoice);
+  console.log('bot : ' + botchoice);
+
+  resetbotcolor(botchoice, '#153243' ,'solid 6px #76d495' );
+
+ if ((playerchoice === 0 && botchoice === 2) || (playerchoice === 1 && botchoice === 0) || (playerchoice === 2 && botchoice === 1) ) {
+  resultat.innerHTML = 'vous avez gagné'
+  resultat.style.color = 'green';
+  victory.innerHTML += '|' 
+ }
+ else if ((playerchoice === 0 && botchoice === 0) || (playerchoice === 1 && botchoice === 1) || (playerchoice === 2 && botchoice === 2) ) {
+  resultat.innerHTML = ' c\'est une égalité'
+  resultat.style.color = 'blue';
+  draw.innerHTML += '|'  
+ }
+ else {
+  resultat.innerHTML = 'vous avez perdu'
+  resultat.style.color = 'red';
+  lose.innerHTML += '|' 
+ }
+
+// if (resultat.innerHTML='gagné') {
+//   victory.innerHTML = i++;
+// }
+// else if (resultat.innerHTML='perdu') {
+//   lose.innerHTML = i++;
+// }
+// else if (resultat.innerHTML='égalité') {
+//   draw.innerHTML = i++;
+// }
+
+})
